11a21f546019ad06082e818c7896bec3b540a1ac
ashleey_ap <antonio123.flako@gmail.com>
2026-03-20 12:45:36 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 onload = () => {
   document.body.classList.remove("container");
-};
+// 🎵 Control de música
+const musica = document.getElementById("musica");
+const btn = document.getElementById("btnMusica");
+
+let reproduciendo = false;
+
+btn.addEventListener("click", () => {
+  if (!reproduciendo) {
+    musica.play();
+    reproduciendo = true;
+    btn.innerHTML = "<span>⏸</span>";
+  } else {
+    musica.pause();
+    reproduciendo = false;
+    btn.innerHTML = "<span>🎵</span>";
+  }
+});
